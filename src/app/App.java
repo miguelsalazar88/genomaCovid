c6c0fa54616aa6a155b4891dd41ceab46b5b0384
Miguel Salazar <miguelsalazar88@gmail.com>
2021-09-27 19:32:52 -0500

    Falta una funci´øn que devuelva todas las posibles combinaciones de CTGA

--- src/app/App.java
+++ src/app/App.java
@@ -1,8 +1,23 @@
 package app;
 
+import modelo.Modelo;
+
+import java.util.LinkedList;
+
 public class App {
 
     public static void main(String[] args) {
+
+        Modelo m = new Modelo();
+        String genoma = m.leerDoc();
+        LinkedList<Character> letras = m.letrasEnDoc(genoma);
+        System.out.println("Letras en el documento: " + letras.toString());
+
+        for (Character c: letras) {
+            m.letraRepetida(c,genoma);
+        }
+
+
 
     }
 
--- src/modelo/Modelo.java
+++ src/modelo/Modelo.java
@@ -1,6 +1,53 @@
 package modelo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.stream.Stream;
+
 public class Modelo {
 
+    public Modelo(){
+
+    }
+
+    public String leerDoc(){
+
+        Path path = Paths.get("/Users/miguelsalazar/git/genomaCovid/src/docs/sequences.fasta");
+        StringBuilder sb = new StringBuilder();
+
+        try{
+            Stream<String> stream = Files.lines(path);
+            stream.forEach(s -> sb.append(s));
+        } catch (IOException e){
+            System.out.println("Pailander");
+        }
+        return sb.toString();
+    }
+
+    public LinkedList<Character> letrasEnDoc(String s){
+        LinkedList<Character> letras = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            if(!letras.contains(s.charAt(i))){
+                letras.add(s.charAt(i));
+            }
+        }
+        return letras;
+    }
+
+    public long letraRepetida(char letra, String s){
+        long num = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)==letra){
+                num++;
+            }
+        }
+        System.out.println("La letra " + letra + " se repite " +  num + " veces en el documento." );
+        return num;
+    }
     
+
 }
